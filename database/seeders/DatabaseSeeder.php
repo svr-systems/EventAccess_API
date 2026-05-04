016e25b388f32e28936da327ff554cf2611afdfd
Er Leví <ivelre123@gmail.com>
2026-05-04 17:35:53 -0600

    Seeds and migtrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,9 +9,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
   public function run(): void {
-    // $this->call(RoleSeeder::class);
-    // $this->call(UserSeeder::class);
-    // $this->call(SaleItemStatusSeeder::class);
+    $this->call(RoleSeeder::class);
+    $this->call(UserSeeder::class);
+    $this->call(SaleItemStatusSeeder::class);
     $this->call(StateSeeder::class);
     $this->call(MunicipalitiesSeeder::class);
     $this->call(FiscalRegimeSeeder::class);
