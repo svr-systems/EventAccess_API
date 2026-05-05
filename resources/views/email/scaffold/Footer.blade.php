86606f13bd53b6278158e49b9d30d61b3a4f9323
Er Leví <ivelre123@gmail.com>
2026-05-04 22:08:57 -0600

    NexoraPro to email footer

--- resources/views/email/scaffold/Footer.blade.php
+++ resources/views/email/scaffold/Footer.blade.php
@@ -4,7 +4,7 @@
     retransmisión o divulgación no autorizada, parcial o total, de su contenido. Si usted no es el destinatario del
     presente correo, por favor elimínelo junto con cualquier documento adjunto que pudiera contener.
     <br><br>
-    El tratamiento de los datos personales por <strong>Nexora</strong> se realiza conforme al Aviso de
+    El tratamiento de los datos personales por <strong>NexoraPro</strong> se realiza conforme al Aviso de
     Privacidad disponible en:
     <br>
     <a href="https://nexorapro.mx" style="color: inherit">nexorapro.mx</a>
